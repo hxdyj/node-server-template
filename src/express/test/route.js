5e0d8319eb9e2a18f399bb9bc01ad95c4ea8ea06
“songle” <songle@leither.org>
2018-02-02 14:08:34 +0800

    add js file upload

--- src/express/test/route.js
+++ src/express/test/route.js
@@ -77,6 +77,7 @@ module.exports = (app, client, mysqlConnect) => {
 		if (!req.files)
 			return res.status(400).send('No files were uploaded.');
 		// The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+		console.log('test upload files:', req.files)
 		let sampleFile = req.files.sampleFile;
 
 		// Use the mv() method to place the file somewhere on your server
